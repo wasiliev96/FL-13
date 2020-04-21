9080a27bada4204b7a40f04d1da3df1e5b6320f8
Vasyl Vasyliev <wasiliev.cloud@gmail.com>
2020-04-22 01:14:50 +0300

    2 task completed

--- FL13_HW6/homework/js/task2.js
+++ FL13_HW6/homework/js/task2.js
@@ -1,1 +1,8 @@
 // Your code goes here
+let word = prompt('Enter word');
+
+function getMiddle(string) {
+    return string.length % 2 ? string.substr(string.length / 2, 1) : string.substr((string.length / 2) - 1, 2);
+}
+
+alert(getMiddle(word));
