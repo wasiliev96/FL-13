d47f2b6c7caa7c458d173681bfbb270c277ce970
Vasyl Vasyliev <wasiliev.cloud@gmail.com>
2020-04-22 14:42:37 +0300

    2 task rewrited

--- FL13_HW6/homework/js/task2.js
+++ FL13_HW6/homework/js/task2.js
@@ -1,8 +1,22 @@
 // Your code goes here
-let word = prompt('Enter word');
+let word = prompt('Enter word(sentence):');
 
-function getMiddle(string) {
-    return string.length % 2 ? string.substr(string.length / 2, 1) : string.substr((string.length / 2) - 1, 2);
+/**
+ *
+ * @param str - user word
+ * @returns {string} - user output
+ */
+function extractMiddle(str) {
+    let position, length;
+    if (str.length === 0) return 'Invalid value';
+    if (str.length % 2 === 1) {
+        position = str.length / 2;
+        length = 1;
+    } else {
+        position = str.length / 2 - 1;
+        length = 2;
+    }
+    return `Your word(sentence): ${str}\nMiddle character(s): \"${str.substring(position, position + length)}\"`;
 }
 
-alert(getMiddle(word));
+alert(extractMiddle(word));
