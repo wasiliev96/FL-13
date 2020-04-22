29ae38839f64bc11ae091531e278f786b370e6ec
Vasyl Vasyliev <wasiliev.cloud@gmail.com>
2020-04-22 15:29:42 +0300

    eslint fixes. No magic numbers!

--- FL13_HW6/homework/js/task1.js
+++ FL13_HW6/homework/js/task1.js
@@ -1,15 +1,17 @@
 // Your code goes here
 const run = () => {
+    const PERCENTAGE_MAX = 100;
+    const DIGITS_AFTER_COMMA = 2;
     let checkValue = parseFloat(prompt('Enter check value: ', '100'));
     if (typeof checkValue === 'number' && checkValue > 0) {
         let percentage = parseFloat(prompt('Enter tip percentage: ', '20')),
-            tip = checkValue * percentage / 100;
-        if (typeof percentage === 'number' && percentage > 0 && percentage < 100) {
+            tip = checkValue * percentage / PERCENTAGE_MAX;
+        if (typeof percentage === 'number' && percentage > 0 && percentage < PERCENTAGE_MAX) {
             alert(
-                `Check value: ${checkValue.toFixed(2)}
-Tip: ${percentage.toFixed(2)}%
-Tip amount:${tip.toFixed(2)}
-Total sum to pay: ${(checkValue + checkValue * percentage / 100).toFixed(2)}`);
+                `Check value: ${checkValue.toFixed(DIGITS_AFTER_COMMA)}
+Tip: ${percentage.toFixed(DIGITS_AFTER_COMMA)}%
+Tip amount:${tip.toFixed(DIGITS_AFTER_COMMA)}
+Total sum to pay: ${(checkValue + checkValue * percentage / PERCENTAGE_MAX).toFixed(DIGITS_AFTER_COMMA)}`);
         } else {
             printError('invalid Percentage');
         }
