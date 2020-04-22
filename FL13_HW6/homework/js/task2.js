9a0af40c2da166426d63616fd38ac727d3c4647a
Vasyl Vasyliev <wasiliev.cloud@gmail.com>
2020-04-22 15:25:45 +0300

    eslint fixes

--- FL13_HW6/homework/js/task2.js
+++ FL13_HW6/homework/js/task2.js
@@ -6,17 +6,21 @@ let word = prompt('Enter word(sentence):');
  * @param str - user word
  * @returns {string} - user output
  */
+const EQUAL_DIVIDER = 2;
+
 function extractMiddle(str) {
     let position, length;
-    if (str.length === 0) return 'Invalid value';
-    if (str.length % 2 === 1) {
-        position = str.length / 2;
+    if (str.length === 0) {
+        return 'Invalid value';
+    }
+    if (str.length % EQUAL_DIVIDER !== 0) {
+        position = str.length / EQUAL_DIVIDER;
         length = 1;
     } else {
-        position = str.length / 2 - 1;
-        length = 2;
+        position = str.length / EQUAL_DIVIDER - 1;
+        length = EQUAL_DIVIDER;
     }
-    return `Your word(sentence): ${str}\nMiddle character(s): \"${str.substring(position, position + length)}\"`;
+    return `Your word(sentence): ${str}\nMiddle character(s): "${str.substring(position, position + length)}"`;
 }
 
 alert(extractMiddle(word));
