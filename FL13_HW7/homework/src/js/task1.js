18dd07d4fb56e097cde59ab9b1f9f407505dc95c
Vasyl Vasyliev <wasiliev.cloud@gmail.com>
2020-04-23 20:55:50 +0300

    user password validate

--- FL13_HW7/homework/src/js/task1.js
+++ FL13_HW7/homework/src/js/task1.js
@@ -2,22 +2,39 @@
 const USERNAME_MIN_LENGTH = 4
 let username = prompt('Enter username: ')
 let password = prompt('Enter password')
-if (!username) {
+if (!isEmpty(username)) {
   alert('Canceled.')
+} else {
+  run(username, password)
 }
-
-run(username, password)
 
 function run (username, password) {
   if (username.length < USERNAME_MIN_LENGTH) {
     alert('I don\'t know any users having name length less than 4 symbols')
   } else {
-    username === 'User' || username === 'Admin' ? validatePassword(password) :
+    username === 'User' || username === 'Admin' ? validatePassword(username,
+      password) :
       alert('I don\'t know you')
   }
 }
 
-let validatePassword = (password) => {
-  console.log(password)
+let validatePassword = (username, password) => {
+  if (isEmpty(password)) {
+    alert('Canceled.')
+  } else {
+    if (username === 'User' && password === 'UserPass' || username ===
+        'Admin' && password === 'RootPass') {
+      greetings(username)
+    } else {
+      alert('Wrong password')
+    }
+  }
 }
 
+function greetings (username) {
+  console.log(username)
+}
+
+function isEmpty (input) {
+  return !input
+}
