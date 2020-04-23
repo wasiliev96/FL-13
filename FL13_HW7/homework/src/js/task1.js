59579107d95d707e395a2234c76f9997072000d8
Vasyl Vasyliev <wasiliev.cloud@gmail.com>
2020-04-23 20:46:12 +0300

    user login validate

--- FL13_HW7/homework/src/js/task1.js
+++ FL13_HW7/homework/src/js/task1.js
@@ -1,1 +1,23 @@
 // Your code goes here
+const USERNAME_MIN_LENGTH = 4
+let username = prompt('Enter username: ')
+let password = prompt('Enter password')
+if (!username) {
+  alert('Canceled.')
+}
+
+run(username, password)
+
+function run (username, password) {
+  if (username.length < USERNAME_MIN_LENGTH) {
+    alert('I don\'t know any users having name length less than 4 symbols')
+  } else {
+    username === 'User' || username === 'Admin' ? validatePassword(password) :
+      alert('I don\'t know you')
+  }
+}
+
+let validatePassword = (password) => {
+  console.log(password)
+}
+
