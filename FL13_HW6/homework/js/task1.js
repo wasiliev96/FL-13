8993b0bb5ef914a37229cef3f826976b0594609f
Vasyl Vasyliev <wasiliev.cloud@gmail.com>
2020-04-23 13:54:09 +0300

    completely override realization. Used .filter() function

--- FL13_HW6/homework/js/task1.js
+++ FL13_HW6/homework/js/task1.js
@@ -1,31 +1,24 @@
-// Your code goes here
-const run = () => {
-    const PERCENTAGE_MAX = 100;
-    const DIGITS_AFTER_COMMA = 2;
-    let checkValue = parseFloat(prompt('Enter check value: ', '100'));
-    if (typeof checkValue === 'number' && checkValue > 0) {
-        let percentage = parseFloat(prompt('Enter tip percentage: ', '20')),
-            tip = checkValue * percentage / PERCENTAGE_MAX;
-        if (typeof percentage === 'number' && percentage > 0 && percentage < PERCENTAGE_MAX) {
-            alert(
-                `Check value: ${checkValue.toFixed(DIGITS_AFTER_COMMA)}
-Tip: ${percentage.toFixed(DIGITS_AFTER_COMMA)}%
-Tip amount:${tip.toFixed(DIGITS_AFTER_COMMA)}
-Total sum to pay: ${(checkValue + checkValue * percentage / PERCENTAGE_MAX).toFixed(DIGITS_AFTER_COMMA)}`);
-        } else {
-            printError('invalid Percentage');
-        }
-    } else {
-        printError('invalid check value');
-    }
-};
+const PERCENTAGE_MAX = 100;
+const DIGITS_AFTER_COMMA = 2;
 
-/**
- *
- * @param errorcode - string, error short description
- */
-function printError(errorcode) {
-    alert('Error: ' + errorcode);
+// removing spaces from user inputs and parse to float
+const INPUT_VALUES_QUANTITY = 2; // for eslint
+let checkValue = parseFloat( prompt( 'Enter check:', '200' ).replace( /\s/g, '' ) );
+let tipPercentage = parseFloat( prompt( 'Enter tip percent:', '15' ).replace( /\s/g, '' ) );
+
+function calculate ( checkValue, tipPercentage ) {
+    // Filter if values are positive and percentage lower than 100
+    return [ checkValue, tipPercentage ].filter( function ( i ) {
+        return i >= 0 && tipPercentage <= PERCENTAGE_MAX;
+        // if filtered array is equal start array - return message
+    } ).length === INPUT_VALUES_QUANTITY
+           ? `Check value: ${ checkValue.toFixed( DIGITS_AFTER_COMMA ) }$\n
+Tip: ${ tipPercentage.toFixed( DIGITS_AFTER_COMMA ) }%\n
+Tip amount: ${ (checkValue * tipPercentage / PERCENTAGE_MAX).toFixed( DIGITS_AFTER_COMMA ) }$\n
+Total sum to pay: ${ (checkValue + checkValue * tipPercentage / PERCENTAGE_MAX).toFixed( DIGITS_AFTER_COMMA ) }$
+`
+        // otherwise, return error
+           : 'Error input';
 }
 
-run();
+alert( calculate( checkValue, tipPercentage ) );
