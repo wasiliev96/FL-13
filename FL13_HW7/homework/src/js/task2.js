1ec9933bb8669c27b6fbe8ab30ea808dbe55b1e4
Vasyl Vasyliev <wasiliev.cloud@gmail.com>
2020-04-26 17:27:23 +0300

    added some comments

--- FL13_HW7/homework/src/js/task2.js
+++ FL13_HW7/homework/src/js/task2.js
@@ -4,6 +4,7 @@ const PRIZE_BASE = 100;
 const PRIZE_MULTIPLIER = 2;
 const USER_ATTEMPTS_MAX = 3;
 
+// init game object and base props
 let game = {
   currentRange: RANDOM_RANGE_STEP,
   guessNumber: 1,
@@ -11,13 +12,20 @@ let game = {
   wallet: 0,
   levelMaxPrize: PRIZE_BASE,
   currentQuizPrice: PRIZE_BASE,
+
+// instead of array, we configure method to calculate current quith price
+//  recalculated every user input
   updateCurrentQuizPrice () {
     this.currentQuizPrice /= PRIZE_MULTIPLIER;
   },
+
+  // set random integer in defined range
   setRandomNumber () {
     this.guessNumber = Math.floor(Math.random() * this.currentRange);
     console.log('game guess number: ' + this.guessNumber); // for debug
   },
+
+// show wallet message and ask user for input number
   askUserNumber () {
     return prompt(`
 Choose a roulette pocket number from 0 to ${ this.currentRange }\n
@@ -26,6 +34,8 @@ Total prize: ${ this.wallet }$
 Possible prize on current attempt: ${ this.currentQuizPrice }$
 `);
   },
+
+  // update game props according to level up
   levelUp () {
     this.wallet += this.currentQuizPrice;
     this.levelMaxPrize *= PRIZE_MULTIPLIER;
@@ -35,10 +45,14 @@ Possible prize on current attempt: ${ this.currentQuizPrice }$
     game.currentRange += RANDOM_RANGE_STEP;
     game.attempts = USER_ATTEMPTS_MAX;
   },
+
+// run quiz while user have attempts
   runQuizz () {
     while (this.attempts > 0) {
       this.setRandomNumber();
       let userChoise = parseInt(this.askUserNumber());
+      // update level if user was right, otherwise = minus attept
+      // and recalculate prizes
       if (userChoise === game.guessNumber) {
         this.levelUp();
         return true;
@@ -50,6 +64,8 @@ Possible prize on current attempt: ${ this.currentQuizPrice }$
     }
     return false;
   },
+
+  // reset stored game props
   reset () {
     this.currentRange = RANDOM_RANGE_STEP;
     this.guessNumber = 1;
@@ -57,6 +73,8 @@ Possible prize on current attempt: ${ this.currentQuizPrice }$
     this.wallet = 0;
     this.currentQuizPrice = PRIZE_BASE;
   },
+
+// main game stream.
   play (choice) {
     let userchoice = choice;
     while (userchoice) {
@@ -73,6 +91,7 @@ Possible prize on current attempt: ${ this.currentQuizPrice }$
         userchoice = confirm(
           `Thank you for your participation. Your prize is: ${ game.wallet }$. Do you want to continue?`
         );
+        // reset data if user loose or exit
         this.reset();
       }
     }
@@ -80,12 +99,14 @@ Possible prize on current attempt: ${ this.currentQuizPrice }$
   }
 };
 
+// main function, program starts here
 function main () {
   let isRun = true;
   let choice = confirm('Do you want to play a game?');
   if (!choice) {
     alert('You did not become a billionaire, but can');
   } else {
+    // Runs quiz while user accept
     while (isRun) {
       isRun = game.play(choice);
     }
