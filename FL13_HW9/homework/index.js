d984dd5d7123731e77c627be41eae1e9a6a4871d
Vasyl Vasyliev <wasiliev.cloud@gmail.com>
2020-04-30 21:29:32 +0300

    task 1 finished

--- FL13_HW9/homework/index.js
+++ FL13_HW9/homework/index.js
@@ -1,1 +1,10 @@
-// Your code goes here
+// task 1: string<=>number converter
+let convert = (input) => {
+  let answer = [];
+  for (let i = 0; i < input.length; i++) {
+    answer.push(
+      typeof input[i] === "number" ? input[i] + "" : parseInt(input[i])
+    );
+  }
+  return answer;
+};
