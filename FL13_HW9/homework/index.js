5ffb3cf34815af9f11e1bd99fd783211c1401add
Vasyl Vasyliev <wasiliev.cloud@gmail.com>
2020-04-30 21:57:53 +0300

    fix logic

--- FL13_HW9/homework/index.js
+++ FL13_HW9/homework/index.js
@@ -1,10 +1,13 @@
 // task 1: string<=>number converter
-let convert = (input) => {
+let convert = (...args) => {
   let answer = [];
-  for (let i = 0; i < input.length; i++) {
-    answer.push(
-      typeof input[i] === "number" ? input[i] + "" : parseInt(input[i])
-    );
+  for (let i = 0; i < args.length; ++i) {
+    if (typeof args[i] == "number") {
+      answer.push(args[i] + "");
+    } else {
+      answer.push(parseInt(args[i]));
+    }
   }
   return answer;
 };
+convert(3, "2"); /*?*/
