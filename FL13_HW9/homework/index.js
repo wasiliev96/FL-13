62a03197af01437f608b764ed135ce651e557d5a
Vasyl Vasyliev <wasiliev.cloud@gmail.com>
2020-04-30 22:03:46 +0300

    task 2 completed

--- FL13_HW9/homework/index.js
+++ FL13_HW9/homework/index.js
@@ -10,4 +10,12 @@ let convert = (...args) => {
   }
   return answer;
 };
-convert(3, "2"); /*?*/
+convert(3, "2");
+
+// task 2: execute function to all array items
+let executeForEach = (arr, handler) => {
+  for (let i = 0; i < arr.length; ++i) {
+    handler(arr[i]);
+  }
+};
+executeForEach([1, 2, 3, 4], (a) => a * 2);
