9ef62bbf207f8d84650c72c2d0b2ce40949f117c
Vasyl Vasyliev <wasiliev.cloud@gmail.com>
2020-05-01 11:10:07 +0300

    added debug comments. fixed execute[F=>f]orEach func name

--- FL13_HW9/homework/index.js
+++ FL13_HW9/homework/index.js
@@ -10,15 +10,19 @@ let convert = (...args) => {
   }
   return answer;
 };
-convert(3, "2");
+
+convert("1", 2, 3, "4"); // [1, '2', '3', 4]
 
 // task 2: execute function to all array items
-let executeForEach = (arr, handler) => {
+let executeforEach = (arr, handler) => {
   for (let i = 0; i < arr.length; ++i) {
     handler(arr[i]);
   }
 };
-executeForEach([1, 2, 3, 4], (a) => a * 2);
+
+executeforEach([1, 2, 3], function (el) {
+  console.log(el * 2);
+}); // 2 4 6
 
 // task 3: return transformed by callback array
 let mapArray = (arr, handler) => {
@@ -31,12 +35,12 @@ let mapArray = (arr, handler) => {
 
 mapArray([2, "5", 8], function (el) {
   return el + 3;
-});
+}); // returns [5, 8, 11]
 
 // task 4: return filtered by callback array. Use func #2
 let filterArray = (arr, handler) => {
   const answer = [];
-  executeForEach(arr, (item) => {
+  executeforEach(arr, (item) => {
     handler(item) && answer.push(item);
   });
   return answer;
@@ -45,11 +49,12 @@ let filterArray = (arr, handler) => {
 filterArray([2, 5, 8], function (el) {
   return el % 2 === 0;
 });
+// returns [2, 8]
 
 // task 5: check if array contains passed $param value. Use func #2
 let containsValue = (arr, param) => {
   let isContains = false;
-  executeForEach(arr, (item) => {
+  executeforEach(arr, (item) => {
     if (item === param) {
       isContains = true;
     }
@@ -57,8 +62,8 @@ let containsValue = (arr, param) => {
   return isContains;
 };
 
-containsValue([2, 5, 8], 2);
-containsValue([12, 4, 6], 5);
+containsValue([2, 5, 8], 2); // returns true
+containsValue([12, 4, 6], 5); // returns false
 
 // task 6: reverse passed string. return(?) it
 let flipOver = (str) => {
@@ -69,7 +74,7 @@ let flipOver = (str) => {
   return answer;
 };
 
-flipOver("hey world");
+flipOver("hey world"); // 'dlrow yeh'
 
 // task 7: create array from range
 let makeListFromRange = (range) => {
@@ -80,12 +85,12 @@ let makeListFromRange = (range) => {
   return list;
 };
 
-makeListFromRange([2, 7]);
+makeListFromRange([2, 7]); // [2, 3, 4, 5, 6, 7]
 
 // task 8: return array of objects values by passed key
 let getArrayOfKeys = (objects, key) => {
   const answer = [];
-  executeForEach(objects, (object) => {
+  executeforEach(objects, (object) => {
     answer.push(object[key]);
   });
   return answer;
@@ -95,17 +100,16 @@ const fruits = [
   { name: "apple", weight: 0.5 },
   { name: "pineapple", weight: 2 },
 ];
-getArrayOfKeys(fruits, "name");
+getArrayOfKeys(fruits, "name"); // returns [‘apple’, ‘pineapple’]
 
 // task 9: replace all numbers <20 and >10 with '*'. return new array. Use #3
 let substitute = (arr) => {
   return mapArray(arr, (item) => (item > 10 && item < 20 && "*") || item);
 };
 
-substitute([58, 14, 48, 12, 31, 19, 10]);
+substitute([58, 14, 48, 12, 31, 19, 10]); // returns [58, '*', 48, '*', 31, '*', 10]
 
 // task 10: return date that was $(amount) days before passed date
-// TODO: fix this task!
 let getPastDay = (date, amount) => {
   const months = [
     "Jan",
@@ -127,9 +131,9 @@ let getPastDay = (date, amount) => {
 };
 
 const date = new Date(2020, 0, 2);
-getPastDay(date, 1);
-getPastDay(date, 2);
-getPastDay(date, 365);
+getPastDay(date, 1); // 1, (1 Jan 2020)
+getPastDay(date, 2); // 31, (31 Dec 2019)
+getPastDay(date, 365); // 2, (2 Jan 2019)
 
 // task 11: return formatted date
 let formatDate = (date) => {
@@ -147,5 +151,5 @@ let formatDate = (date) => {
   return `${year}/${month}/${day} ${hours}:${minutes}`;
 };
 
-formatDate(new Date("6/15/2019 09:15:00")); // "2018/06/15 09:15"
+formatDate(new Date("6/15/2019 09:15:00")); // "2019/06/15 09:15"
 formatDate(new Date()); // "2020/04/07 12:56" // gets current local time
