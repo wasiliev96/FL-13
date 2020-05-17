b932f51abb4a29ca000f292df4927dd41ca7aa74
Vasyl Vasyliev <wasiliev.cloud@gmail.com>
2020-05-17 23:11:26 +0300

    all has been overrided completely.Git log in separate project (history)

--- FL13_HW12/homework/app.js
+++ FL13_HW12/homework/app.js
@@ -1,250 +1,381 @@
-const root = document.getElementById('root');
-
-const books = [
-  {
-    id: 0,
-    name: 'Unlocking Android',
-    'authors': ['W. Frank Ableson', 'Charlie Collins', 'Robi Sen'],
-    'imageUrl': 'https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/ableson.jpg',
-    'plot': `Unlocking Android: A Developer's Guide provides concise, hands-on instruction 
-    for the Android operating system and development tools. This book teaches important architectural
-     concepts in a straightforward writing style and builds on this with practical and useful 
-     examples throughout.`
-
-  },
-  {
-    id: 1,
-    name: 'Android in Action, Second Edition',
-    'authors': ['W. Frank Ableson', 'Robi Sen'],
-    'imageUrl': 'https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/ableson2.jpg',
-    'plot': `Android in Action, Second Edition is a comprehensive tutorial for Android developers. 
-    Taking you far beyond "Hello Android," this fast-paced book puts you in the driver's seat 
-    as you learn important architectural concepts and implementation strategies. You'll master 
-    the SDK, build WebKit apps using HTML 5, and even learn to extend or replace Android's built-in 
-    features by building useful and intriguing examples. `
-
-
-  },
-  {
-    id: 2,
-    name: 'Specification by Example',
-    'authors': ['Gojko Adzic'],
-    'imageUrl': 'https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/adzic.jpg',
-    'plot': `Lorem ipsum dolor sit amet.`
-  }
-];
-
-const node = (nodeName) => {
-  return document.createElement(nodeName);
-};
-const nodeClass = (tagName, className) => {
-  const _node = node(tagName);
-  _node.className = className;
-  return _node;
-};
-const nodeId = (tagName, id) => {
-  const _node = node(tagName);
-  _node.id = id;
-  return _node;
-};
-const getId = (id) => {
+let booksLibrary = JSON.parse(localStorage.getItem('books'));
+const getId = id => {
   return document.getElementById(id);
 };
-
-
-function App() {
-  let _activeCardId = null;
-  let isPreviewActive = false;
-  let _userBook = {};
-  let _previousListItem = null;
-
-  function setActiveCardId(id) {
-    _activeCardId = id;
+const createNode = (type, id = null, className = null, innerNode = null) => {
+  const node = document.createElement(type);
+  if (className) {
+    node.className = className;
   }
-
-  const _app = document.createElement('div');
-  _app.id = 'app';
-  let _books = JSON.parse(localStorage.getItem('books')) || books;
-
-  window.addEventListener('unload', event => {
-    localStorage.setItem('books', JSON.stringify(_books));
-    console.log('books load to localstorage');
-
-  });
-
-
-  function editCard(bookId) {
-    setModal(_books[bookId]);
+  if (id) {
+    node.id = id;
   }
-
-  function setModal(bookObj) {
-    const _modal = getId('modal');
-    const _bookName = getId('modalName');
-    _bookName.value = bookObj.name;
-    const _bookImage = getId('modalImage');
-    _bookImage.value = bookObj.imageUrl;
-    const _bookAuthors = getId('modalAuthors');
-    _bookAuthors.value = bookObj.authors;
-    const _bookPlot = getId('modalPlot');
-    _bookPlot.innerText = bookObj.plot;
-    _modal.style.display = 'flex';
+  if (innerNode) {
+    node.appendChild(innerNode);
   }
+  return node;
+};
+const root = getId('root');
+const booksWrapper = createNode('div', 'booksWrapper');
+const books = createNode('ul', 'books');
+booksWrapper.appendChild(books);
+const addBookBtn = createNode('button', 'addBook');
+addBookBtn.innerHTML = 'Add Book';
+booksWrapper.appendChild(addBookBtn);
 
-  function generateSkeleton() {
-//  app
-    const _container = nodeClass('div', 'container');
+const previewBox = createNode('div', 'previewBox');
+const preview = createNode('div', 'preview');
+const previewBookName = createNode('h1', 'previewBookName');
+preview.appendChild(previewBookName);
+const previewBookImage = createNode('img', 'previewBookImage');
+previewBookImage.setAttribute('src', '#');
+previewBookImage.setAttribute('alt', 'book image');
+preview.appendChild(previewBookImage);
+const previewBookAuthors = createNode('p', 'previewBookAuthors');
+preview.appendChild(previewBookAuthors);
+const previewBookPlot = createNode('p', 'previewBookPlot');
+preview.appendChild(previewBookPlot);
+previewBox.appendChild(preview);
 
-    const _bookList = nodeId('ul', 'books');
-    _container.appendChild(_bookList);
+const editView = createNode('div', 'editView');
+editView.className = 'hidden';
 
-    const _lightBox = nodeId('div', 'lightBox');
-    _container.appendChild(_lightBox);
+const editNameLabel = createNode('label');
+editNameLabel.appendChild(document.createTextNode('Book name:'));
+const editBookNameInput = createNode('input', 'editBookNameInput');
+editBookNameInput.setAttribute('type', 'text');
+editNameLabel.appendChild(editBookNameInput);
+editView.appendChild(editNameLabel);
 
-    const _card = nodeId('div', 'card');
-    _lightBox.appendChild(_card);
+const editImageLabel = createNode('label');
+editImageLabel.appendChild(document.createTextNode('Book image:'));
+const editImageName = createNode('input', 'editBookImageInput');
+editImageLabel.setAttribute('type', 'text');
+editImageLabel.appendChild(editImageName);
+editView.appendChild(editImageLabel);
 
-    _app.appendChild(_container);
+const editAuthorsLabel = createNode('label');
+editAuthorsLabel.appendChild(document.createTextNode('authors:'));
+const editAuthorsName = createNode('input', 'editBookAuthorsInput');
+editAuthorsLabel.setAttribute('type', 'text');
+editAuthorsLabel.appendChild(editAuthorsName);
+editView.appendChild(editAuthorsLabel);
 
+const editPlotLabel = createNode('label');
+editPlotLabel.appendChild(document.createTextNode('plot:'));
+const editPlotName = createNode('textarea', 'editBookPlotInput');
+editPlotLabel.appendChild(editPlotName);
+editView.appendChild(editPlotLabel);
 
-    // card
-    const _image = nodeId('img', 'cardImage');
-    _image.setAttribute('src', '#');
-    _card.appendChild(_image);
+const editControls = createNode('div', null, 'edit-control');
+const editCancelBtn = createNode('button', 'editCancel', 'cancel-btn');
+editCancelBtn.innerHTML = 'Cancel';
+const editSaveBtn = createNode('button', 'editSave', 'save-btn');
+editSaveBtn.innerHTML = 'Save';
+editSaveBtn.addEventListener('click', function () {
+  saveEdit(this.dataset.target);
+});
+editControls.appendChild(editCancelBtn);
+editControls.appendChild(editSaveBtn);
+editView.appendChild(editControls);
+previewBox.appendChild(editView);
 
-    const _name = nodeId('h2', 'cardName');
-    _card.appendChild(_name);
+const addView = createNode('div', 'add');
+addView.className = 'hidden';
 
-    const _authors = nodeId('p', 'cardAuthors');
-    _card.appendChild(_authors);
+const addNameLabel = createNode('label');
+addNameLabel.appendChild(document.createTextNode('Book name:'));
+const addBookName = createNode('input', 'addBookNameInput');
+addBookName.setAttribute('type', 'text');
+addNameLabel.appendChild(addBookName);
+addView.appendChild(addNameLabel);
 
-    const _plot = nodeId('p', 'cardPlot');
-    _card.appendChild(_plot);
+const addImageLabel = createNode('label');
+addImageLabel.appendChild(document.createTextNode('Book image:'));
+const addImageName = createNode('input', 'addBookImageInput');
+addImageLabel.setAttribute('type', 'text');
+addImageLabel.appendChild(addImageName);
+addView.appendChild(addImageLabel);
 
-    const _buttons = nodeClass('div', 'buttons');
-    _card.appendChild(_buttons);
+const addAuthorsLabel = createNode('label');
+addAuthorsLabel.appendChild(document.createTextNode('authors:'));
+const addAuthorsName = createNode('input', 'addBookAuthorsInput');
+addAuthorsLabel.setAttribute('type', 'text');
+addAuthorsLabel.appendChild(addAuthorsName);
+addView.appendChild(addAuthorsLabel);
 
-    const _editBtn = nodeId('button', 'editBtn');
-    _editBtn.innerText = 'Edit';
-    _editBtn.addEventListener('click', function () {
-      console.log('edit button');
-      editCard(_activeCardId);
-    });
-    _buttons.appendChild(_editBtn);
+const addPlotLabel = createNode('label');
+addPlotLabel.appendChild(document.createTextNode('plot:'));
+const addPlotName = createNode('textarea', 'addBookPlotInput');
+addPlotLabel.appendChild(addPlotName);
+addView.appendChild(addPlotLabel);
 
-    // modal
-    const _modal = nodeId('div', 'modal');
+const addControls = createNode('div', null, 'add-control');
+const addCancelBtn = createNode('button', 'addCancel', 'cancel-btn');
+addCancelBtn.innerHTML = 'Cancel';
+const addSaveBtn = createNode('button', 'addSave', 'save-btn');
+addSaveBtn.innerHTML = 'Save';
+addControls.appendChild(addCancelBtn);
+addControls.appendChild(addSaveBtn);
+addView.appendChild(addControls);
+previewBox.appendChild(addView);
+root.appendChild(booksWrapper);
+root.appendChild(previewBox);
 
-    const _modalName = nodeId('input', 'modalName');
-
-    const _modalImage = nodeId('input', 'modalImage');
-
-    const _modalAuthors = nodeId('input', 'modalAuthors');
-
-    const _modalPlot = nodeId('textarea', 'modalPlot');
-
-    const _modalButtons = nodeClass('div', 'buttons');
-    const _cancelBtn = nodeId('button', 'modalCancel');
-    _cancelBtn.innerText = 'Cancel';
-    const _saveBtn = nodeId('button', 'modalSave');
-    _saveBtn.innerText = 'Save';
-    _modalButtons.appendChild(_cancelBtn);
-    _modalButtons.appendChild(_saveBtn);
-
-    _modal.appendChild(_modalName);
-    _modal.appendChild(_modalImage);
-    _modal.appendChild(_modalAuthors);
-    _modal.appendChild(_modalPlot);
-    _modal.appendChild(_modalButtons);
-    document.body.appendChild(_modal);
-
-    function cancelHandler() {
-      _modal.style.display = 'none';
-    }
-
-    _cancelBtn.addEventListener('click', cancelHandler), {once: true};
-
-    _saveBtn.addEventListener('click', () => {
-      _userBook.id = _activeCardId;
-      _userBook.name = _modalName.value || 'No name';
-      _userBook.imageUrl = _modalImage.value;
-      _userBook.authors = _modalAuthors.value;
-      _userBook.plot = _modalPlot.value;
-      _books[_activeCardId] = _userBook;
-      console.log(_books[_activeCardId]);
-      const relativeLi = getId(`listItem${_activeCardId}`);
-      relativeLi.innerText = _userBook.name;
-
-      if (!_userBook) {
-        alert('Book cannot be empty!');
-      }
-      _userBook = {};
-      alert('saved');
-      updateCard(_books[_activeCardId]);
-      _modal.style.display = 'none';
-    });
-    _modal.style.display = 'flex';
-  }
-
-
-  function pushApp() {
-    root.appendChild(_app);
-  }
-
-  function updateCard(item) {
-    getId('cardImage').setAttribute('src', item.imageUrl || '#');
-    getId('cardName').innerText = item.name || 'Unnamed title';
-    getId('cardAuthors').innerText = item.authors || 'Author is unnamed';
-    getId('cardPlot').innerText = item.plot || 'Empty...';
-  }
-
-  function pushList() {
-
-    const _bookParent = document.querySelector('#books');
-
-
-    const createListItem = (item) => {
-      const _node = node('li');
-      _node.id = `listItem${item.id}`
-      ;
-      const _name = document.createTextNode(item.name);
-      _node.appendChild(_name);
-
-      _node.addEventListener('click', function () {
-        if (!_previousListItem) {
-          _previousListItem = _node;
-        } else {
-          _previousListItem.classList.remove('active');
-          _previousListItem = _node;
-        }
-        _node.classList.add('active');
-        setActiveCardId(item.id);
-        if (!isPreviewActive) {
-          isPreviewActive = true;
-          getId('card').style.display = 'flex';
-        }
-        updateCard(_books[item.id]);
-        console.log(
-          `active item id: ${item.id}`
-        );
-      });
-      return _node;
-    };
-
-    for (const book of _books) {
-      const _item = createListItem(book);
-      _bookParent.appendChild(_item);
-    }
-  }
-
-  return {
-    generateSkeleton: generateSkeleton,
-    pushApp: pushApp,
-    pushList: pushList
-  };
+function updatebookList() {
+  books.innerHTML = booksLibrary
+    .map(
+      (book, index) =>
+        `<li>
+        <a href='index.html?id=${index}' onclick = 'linkHandler(event)'>${book.name}</a>
+        <button class='edit-book-li' onclick="edit(event)">edit</button>
+        </li>`
+    )
+    .join('');
 }
 
-const app = new App();
-app.generateSkeleton();
-app.pushApp();
-app.pushList();
-console.log('done');
+updatebookList();
+
+function switchView(view, book, bookId) {
+  switch (view) {
+    case edit:
+      editBookNameInput.value = book.name || '';
+      editImageName.value = book.image || '';
+      editAuthorsName.value = book.authors || '';
+      editPlotName.innerText = book.plot || '';
+      editSaveBtn.dataset.target = bookId;
+      setActiveView(editView);
+      break;
+    default:
+      previewBookName.innerHTML = book.name || 'No name';
+      previewBookImage.setAttribute('src', book.image || '');
+      previewBookAuthors.innerHTML = book.authors || 'Unknown authors';
+      previewBookPlot.innerHTML = book.plot || 'No plot';
+      setActiveView(preview);
+      break;
+  }
+}
+
+function updatePreview() {
+  const bookId = +location.search.match(/\d*$/)[0];
+  console.log('updatePreview=>bookId: ' + bookId);
+  const book = booksLibrary[bookId];
+  if (!book) {
+    return;
+  }
+  switch (location.hash) {
+    case '#edit':
+      switchView(edit, book, bookId);
+      break;
+    default:
+      switchView(preview, book);
+      break;
+  }
+}
+
+function linkHandler(e) {
+  e.preventDefault();
+  let state = {
+    page: e.target.href
+  };
+  if (addBookBtn.disabled) {
+    addBookBtn.disabled = false;
+  }
+  console.log(`state page: ${state.page}`);
+
+  history.pushState(state, '', state.page + '#preview');
+  //   console.log(+state.page.match(/\d*$/)[0]);
+  setActiveView(preview);
+  updatePreview();
+  console.log(`book id: ${+location.search.match(/\d*$/)}`);
+}
+
+window.addEventListener('popstate', function () {
+  updatePreview();
+});
+
+// window.onbeforeunload = function () {
+//   if (unloadWarning) {
+//     return `You have made changes on this page that you have not yet
+// confirmed. If you navigate away from this page you will lose your unsaved
+// changes`; } };
+
+window.onunload = function () {
+  // const localPath = location.pathname || '';
+  // localStorage.setItem('urlData', localPath);
+  // localStorage.setItem('state', history.state || '');
+  localStorage.setItem('books', JSON.stringify(booksLibrary));
+  // location = location.origin;
+};
+
+function updateSearch(book) {
+  previewBookName.innerHTML = book.name || 'No name';
+  previewBookImage.setAttribute('src', book.image || '');
+  previewBookAuthors.innerHTML = book.authors || 'Unknown authors';
+  previewBookPlot.innerHTML = book.plot || 'No plot';
+  updatePreview();
+}
+
+window.onload = function () {
+  // const hash = location.hash;
+  const UNWANTED_HASH_CHARS_LENGTH = 4;
+  let searchId = location.search.slice(UNWANTED_HASH_CHARS_LENGTH) || null;
+  console.log('window.onload -> searchId ', searchId);
+
+  if (searchId && searchId < booksLibrary.length) {
+    updateSearch(booksLibrary[searchId]);
+  } else {
+    history.pushState('', '', '/index.html');
+  }
+  updatePreview();
+};
+
+function validateEditInput() {
+  if (editBookNameInput.value.length <= 0) {
+    alert('invalid Book Title!');
+    return false;
+  }
+  if (editBookNameInput.value.length <= 0) {
+    alert(addBookName.value);
+    alert('invalid image path!');
+    return false;
+  }
+  if (editAuthorsName.value.length <= 0) {
+    alert('invalid author name!');
+    return false;
+  }
+  if (editPlotName.value.length <= 0) {
+    alert('invalid plot!');
+    return false;
+  }
+  return true;
+}
+
+function edit(event) {
+  addBookBtn.disabled = true;
+
+  const bookId = +event.target.previousElementSibling.href.match(/\d*$/)[0];
+  console.log('edit -> bookId', bookId);
+  const book = booksLibrary[bookId];
+
+  history.pushState('', '', event.target.previousElementSibling.href + '#edit');
+  setActiveView(editView);
+  editBookNameInput.value = book.name || '';
+  editImageName.value = book.image || '';
+  editAuthorsName.value = book.authors || '';
+  editPlotName.innerText = book.plot || '';
+  editSaveBtn.dataset.target = bookId + '';
+}
+
+addBookBtn.addEventListener('click', addBookView);
+
+function addBookView() {
+  addBookBtn.disabled = true;
+  setActiveView(addView);
+  const locationString = './index.html' + '#add';
+  history.pushState('', '', locationString);
+  preview.classList.add('hidden');
+  addView.classList.remove('hidden');
+  addBookName.value = '';
+  addImageName.value = '';
+  addAuthorsName.value = '';
+  addPlotName.value = '';
+
+  addSaveBtn.addEventListener('click', saveBtnHandler);
+}
+
+function validateInput() {
+  if (addBookName.value.length <= 0) {
+    alert('invalid Book Title!');
+    return false;
+  }
+  if (addImageName.value.length <= 0) {
+    alert('invalid image path!');
+    return false;
+  }
+  if (addAuthorsName.value.length <= 0) {
+    alert('invalid author name!');
+    return false;
+  }
+  if (addPlotName.value.length <= 0) {
+    alert('invalid plot!');
+    return false;
+  }
+  return true;
+}
+
+function saveBtnHandler() {
+  if (!validateInput()) {
+    return;
+  }
+  addBookBtn.disabled = false;
+  booksLibrary.push({
+    name: addBookName.value,
+    image: addImageName.value,
+    authors: addAuthorsName.value,
+    plot: addPlotName.value
+  });
+  updatebookList();
+  updatePreview();
+  setActiveView(preview);
+  addSaveBtn.removeEventListener('click', saveBtnHandler);
+}
+
+const cancelBtns = document.getElementsByClassName('cancel-btn');
+[...cancelBtns].map(btn => {
+  btn.addEventListener('click', function () {
+    if (confirm('Discard changes?')) {
+      addBookBtn.disabled = false;
+      history.back();
+    }
+  });
+
+  //?Fix
+  return 0;
+});
+
+function saveEdit(target) {
+  if (!validateEditInput()) {
+    return;
+  }
+  addBookBtn.disabled = false;
+  booksLibrary[target].name = editBookNameInput.value;
+  console.log(
+    'saveEdit -> booksLibrary[target].name',
+    booksLibrary[target].name
+  );
+
+  booksLibrary[target].image = editImageName.value;
+  console.log(
+    'saveEdit -> booksLibrary[target].image',
+    booksLibrary[target].image
+  );
+
+  booksLibrary[target].authors = editAuthorsName.value;
+  console.log(
+    'saveEdit -> booksLibrary[target].authors',
+    booksLibrary[target].authors
+  );
+  booksLibrary[target].plot = editPlotName.value;
+  console.log(
+    'saveEdit ->   booksLibrary[target].plot',
+    booksLibrary[target].plot
+  );
+  history.back();
+  updatebookList();
+  updatePreview();
+  const ALERT_TIMEOUT = 300;
+  setTimeout(() => {
+    alert('Book successfully updated!');
+  }, ALERT_TIMEOUT);
+}
+
+function setActiveView(visibleView) {
+  [preview, addView, editView].map(view => {
+    if (!view.classList.contains('hidden')) {
+      view.classList.add('hidden');
+    }
+    visibleView.classList.remove('hidden');
+    return true;
+  });
+}
--- FL13_HW12/homework/books.js
+++ FL13_HW12/homework/books.js
@@ -1,26 +1,32 @@
-// const books = [
-//   {
-//     id: 0,
-//     name: "Unlocking Android",
-//     "authors": ["W. Frank Ableson", "Charlie Collins", "Robi Sen"],
-//     "imageUrl": "https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/ableson.jpg",
-//     "plot": `Unlocking Android: A Developer's Guide provides concise, hands-on instruction for the Android operating system and development tools. This book teaches important architectural concepts in a straightforward writing style and builds on this with practical and useful examples throughout.`
-//
-//   },
-//   {
-//     id: 1,
-//     name: "Android in Action, Second Edition",
-//     "authors": ["W. Frank Ableson", "Robi Sen"],
-//     "imageUrl": "https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/ableson2.jpg",
-//     "plot": `Android in Action, Second Edition is a comprehensive tutorial for Android developers. Taking you far beyond "Hello Android," this fast-paced book puts you in the driver's seat as you learn important architectural concepts and implementation strategies. You'll master the SDK, build WebKit apps using HTML 5, and even learn to extend or replace Android's built-in features by building useful and intriguing examples. `
-//
-//
-//   },
-//   {
-//     id: 2,
-//     name: "Specification by Example",
-//     "authors": ["Gojko Adzic"],
-//     "imageUrl": "https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/adzic.jpg",
-//
-//   }
-// ];
+const booksStore = [
+  {
+    name: 'Unlocking Android',
+    authors: ['W. Frank Ableson', 'Charlie Collins', 'Robi Sen'],
+    image:
+      'https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/ableson.jpg',
+    plot: `Unlocking Android: A Developer's Guide provides concise, hands-on instruction
+     for the Android operating system and development tools. This book teaches important
+      architectural concepts in a straightforward writing style and builds on this with 
+      practical and useful examples throughout.`
+  },
+  {
+    name: 'Android in Action, Second Edition',
+    authors: ['W. Frank Ableson', 'Robi Sen'],
+    image:
+      'https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/ableson2.jpg',
+    plot: `Android in Action, Second Edition is a comprehensive tutorial for Android 
+    developers. Taking you far beyond "Hello Android," this fast-paced book puts you 
+    in the driver's seat as you learn important architectural concepts and implementation
+     strategies. You'll master the SDK, build WebKit apps using HTML 5, and even learn to 
+     extend or replace Android's built-in features by building useful and intriguing examples. `
+  },
+  {
+    name: 'Specification by Example',
+    authors: ['Gojko Adzic'],
+    image:
+      'https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/adzic.jpg'
+  }
+];
+if (!localStorage.getItem('books')) {
+  localStorage.setItem('books', JSON.stringify(booksStore));
+}
