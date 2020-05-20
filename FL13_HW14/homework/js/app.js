4c777e07d0fae4bcee7917221960da0686bb4031
Vasyl Vasyliev <wasiliev.cloud@gmail.com>
2020-05-20 19:02:24 +0300

    suppose it works

--- FL13_HW14/homework/js/app.js
+++ FL13_HW14/homework/js/app.js
@@ -1,11 +1,12 @@
 // Your code goes here
 //@ts-check
-
 /**
  *
  * @param {string} name
  * @param {string} email
  * @constructor
+ * @return {{getName: (function(): string), getEmail: (function(): string),
+ *   addHomeWorkResult: addHomeWorkResult}}
  */
 function Student(name, email) {
   const _name = name;
@@ -22,9 +23,14 @@ function Student(name, email) {
   const addHomeWorkResult = (topic, success) => {
     _homeworkResults.push({ topic, success });
   };
+  const getHomeworkResults = () => {
+    console.log(_homeworkResults);
+  };
   return {
     getName: getName,
     getEmail: getEmail,
+    addHomeWorkResult: addHomeWorkResult,
+    getHomeworkResults: getHomeworkResults,
   };
 }
 
@@ -38,12 +44,13 @@ function Student(name, email) {
 function FrontendLab(students, failedLimit) {
   let _failedHomeworksLimit = failedLimit;
   const _studentsList = students;
+  const _homeworkResults = [];
 
   const printStudentsList = () => {
-    listOfStudents.map((student) => {
+    _studentsList.map((student) => {
       console.log(`name: ${student.name}, email: ${student.email}`);
       const studentResults = [];
-      homeworkResults.map((homeworkResult) => {
+      _homeworkResults.map((homeworkResult) => {
         const record = {};
         record.topic = homeworkResult.topic;
         record.success = homeworkResult.results.find(
@@ -54,7 +61,36 @@ function FrontendLab(students, failedLimit) {
       console.log(studentResults);
     });
   };
+
+  /**
+   *
+   * @param {{string:topic}, [{email: string, success:boolean}]} homeworkResults
+   */
+  const addHomeworkResults = (homeworkResults) => {
+    _homeworkResults.push(homeworkResults);
+  };
+
+  const printStudentsEligibleForTest = () => {
+    _studentsList.forEach((studentRecord) => {
+      let studentFailsCounter = 0;
+      _homeworkResults.forEach((homeworkRecord) => {
+        const records = homeworkRecord.results.filter((result) => {
+          return result.email === studentRecord.email;
+        });
+        records.forEach((record) => {
+          record.success || studentFailsCounter++;
+        });
+      });
+      if (studentFailsCounter < failedLimit) {
+        console.log(
+          `name: ${studentRecord.name}, email: ${studentRecord.email}, counter: ${studentFailsCounter}`
+        );
+      }
+    });
+  };
   return {
     printStudentsList: printStudentsList,
+    addHomeworkResults: addHomeworkResults,
+    printStudentsEligibleForTest: printStudentsEligibleForTest,
   };
 }
