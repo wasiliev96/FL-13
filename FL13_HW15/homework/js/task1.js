17eb072d60bdb2ca6167063aeff438e3b83989e8
Vasyl Vasyliev <wasiliev.cloud@gmail.com>
2020-05-21 21:24:30 +0300

    assign method added

--- FL13_HW15/homework/js/task1.js
+++ FL13_HW15/homework/js/task1.js
@@ -1,1 +1,28 @@
-// TODO: Your code goes here
+function assign(target, letArgs) {
+  if (target == null) {
+    throw new TypeError("Cannot convert (undefined|null) to object");
+  }
+
+  const to = Object(target);
+
+  for (let index = 1; index < arguments.length; index++) {
+    const nextSource = arguments[index];
+
+    if (nextSource != null) {
+      // Skip if undefined or null
+      for (let nextKey in nextSource) {
+        // Avoid bugs when hasOwnProperty is shadowed
+        if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
+          to[nextKey] = nextSource[nextKey];
+        }
+      }
+    }
+  }
+  return to;
+}
+
+const paymentCard = { cash: "100$" };
+const creditCard = { creditLimit: "50$", cash: "100$" };
+const universalCard = assign({}, creditCard, paymentCard); //=>{creditLimit:'50$',
+// 'cash:100$'}
+console.log(universalCard);
