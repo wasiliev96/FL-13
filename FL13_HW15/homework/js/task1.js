531df680cfc275d786e52d244a3f56aea642b22d
Vasyl Vasyliev <wasiliev.cloud@gmail.com>
2020-05-24 15:00:40 +0300

    eslint errors fix. Removed unnecessary calls

--- FL13_HW15/homework/js/task1.js
+++ FL13_HW15/homework/js/task1.js
@@ -1,6 +1,6 @@
-function assign(target, letArgs) {
-  if (target == null) {
-    throw new TypeError("Cannot convert (undefined|null) to object");
+function assign(target) {
+  if (target === null) {
+    throw new TypeError('Cannot convert (undefined|null) to object');
   }
 
   const to = Object(target);
@@ -8,10 +8,8 @@ function assign(target, letArgs) {
   for (let index = 1; index < arguments.length; index++) {
     const nextSource = arguments[index];
 
-    if (nextSource != null) {
-      // Skip if undefined or null
+    if (nextSource !== null) {
       for (let nextKey in nextSource) {
-        // Avoid bugs when hasOwnProperty is shadowed
         if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
           to[nextKey] = nextSource[nextKey];
         }
@@ -20,9 +18,4 @@ function assign(target, letArgs) {
   }
   return to;
 }
-//
-// const paymentCard = { cash: "100$" };
-// const creditCard = { creditLimit: "50$", cash: "100$" };
-// const universalCard = assign({}, creditCard, paymentCard); //=>{creditLimit:'50$',
-// // 'cash:100$'}
-// console.log(universalCard);
+
