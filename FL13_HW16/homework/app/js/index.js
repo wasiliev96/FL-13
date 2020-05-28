98f39271baa224f870f22335aa406a5577fbd053
Vasyl Vasyliev <wasiliev.cloud@gmail.com>
2020-05-28 12:48:54 +0300

    Revert "hw16 init"
    This reverts commit 9b12a8b8c31127ca4d4b5936292bfb2674849da1.

--- FL13_HW16/homework/app/js/index.js
+++
@@ -1,4 +0,0 @@
-const baseUrl = 'http://localhost:3000';
-const appContainer = document.getElementById('app-container');
-
-// Your code goes here
--- FL13_HW16/homework/server/utils/fake-delay.js
+++
@@ -1,8 +0,0 @@
-module.exports = function (min = 0, max = 0) {
-  const waitingTime = Math.floor(Math.random() * (max - min + 1)) + min;
-  return new Promise(resolve => {
-    setTimeout(_ => {
-      resolve();
-    }, waitingTime);
-  });
-}
