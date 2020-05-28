24f8e8ef2d604460ac689633cc7caff9307c2820
Vasyl Vasyliev <wasiliev.cloud@gmail.com>
2020-05-28 14:52:14 +0300

    base UI, get&draw table from xhr

--- FL13_HW16/homework/app/js/index.js
+++ FL13_HW16/homework/app/js/index.js
@@ -1,4 +1,65 @@
-const baseUrl = 'http://localhost:3000';
-const appContainer = document.getElementById('app-container');
-
+const baseUrl = "http://localhost:3000";
+const appContainer = document.getElementById("app-container");
+const usersList = document.getElementById("usersList");
 // Your code goes here
+usersList.innerText = "Loading...";
+function createNode(
+  tagName,
+  nodeClass = null,
+  nodeId = null,
+  innerText = null
+) {
+  const _node = document.createElement(tagName);
+  if (nodeClass) {
+    _node.className = nodeClass;
+  }
+  if (nodeId) {
+    _node.id = nodeId;
+  }
+  if (innerText) {
+    _node.appendChild(document.createTextNode(innerText));
+  }
+  return _node;
+}
+
+const xhr = new XMLHttpRequest();
+
+xhr.open("GET", `${baseUrl}/users`, true);
+
+xhr.send(); // (1)
+
+xhr.onreadystatechange = function () {
+  // (3)
+  if (xhr.readyState !== 4) return;
+
+  if (xhr.status !== 200) {
+    alert(xhr.status + ": " + xhr.statusText);
+  } else {
+    //main treat
+    fillTable(JSON.parse(xhr.responseText));
+  }
+};
+
+function fillTable(data) {
+  const container = document.createDocumentFragment();
+  for (let user of data) {
+    const _li = createNode("li");
+    const userId = createNode("div", "user-id", null, user.id);
+    _li.appendChild(userId);
+    const uname = createNode("div", "name", null, user.name);
+    _li.appendChild(uname);
+    const username = createNode("div", "username", null, user.username);
+    _li.appendChild(username);
+    const _buttonsGroup = createNode("div", "buttonsGroup", null);
+
+    const btnUpdate = createNode("button", "update", null, "Update");
+    _buttonsGroup.appendChild(btnUpdate);
+    const btnDelete = createNode("button", "delete", null, "Delete");
+    _buttonsGroup.appendChild(btnDelete);
+
+    _li.appendChild(_buttonsGroup);
+    container.appendChild(_li);
+  }
+  usersList.innerText = "";
+  usersList.appendChild(container);
+}
